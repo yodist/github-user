d4ffe52f2326dd91f4fdfeba9e4cb3f2578d2c30
yodist <yodisatriani@live.com>
2017-02-04 18:50:08 +0700

    Added style and ng-show

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ var username = "yodist";
 
 
 gitUI.controller('githubData', ['$scope', '$http', function($scope, $http) {
+  
   $scope.find = function() {
+    $scope.display = function() {
+      return true;
+    };
     username = $scope.username;
     $http.get("https://api.github.com/users/" + username)
       .success(function(data) {
