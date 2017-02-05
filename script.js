327475d46969a147449b7341946fb751d869e377
yodist <yodisatriani@live.com>
2017-02-05 23:58:35 +0700

    added blog, followers, following, join date, last activity

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 var gitUI = angular.module('app', []);
 var username = "yodist";
-
+var date = "";
 
 gitUI.controller('githubData', ['$scope', '$http', function($scope, $http) {
-  
+
   $scope.find = function() {
     $scope.display = function() {
       return true;
