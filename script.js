791191dc0f9b4761bf181ce21146e2e926d53fee
yodist <yodisatriani@live.com>
2017-02-09 02:29:46 +0700

    added repos and some other stuffs

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 var gitUI = angular.module('app', []);
 var username = "yodist";
-var date = "";
 
 gitUI.controller('githubData', ['$scope', '$http', function($scope, $http) {
 
@@ -14,7 +13,14 @@ gitUI.controller('githubData', ['$scope', '$http', function($scope, $http) {
     $http.get("https://api.github.com/users/" + username)
       .success(function(data) {
         $scope.userData = data;
+        loadRepos();
       });
+    var loadRepos = function() {
+      $http.get($scope.userData.repos_url)
+        .success(function(data){
+          $scope.repos = data;
+        });
+    };
   };
 
 }]);
